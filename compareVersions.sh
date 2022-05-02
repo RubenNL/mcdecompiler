ead7e2f0a06306c3d281f0899561a857999b3174
Ruben <ruben@rubend.nl>
2022-05-02 19:59:39 +0200

    ignore files.

--- compareVersions.sh
+++ compareVersions.sh
@@ -3,4 +3,6 @@ new=$2
 ./download.sh $old &
 ./download.sh $new &
 wait
-diff --brief --recursive downloads/{$old,$new}/decompiled > changedFiles
+diff --brief --recursive downloads/{$old,$new}/decompiled | grep -v world | grep -v gui | grep -v render | grep -v dispenser > changedFiles
+#                       'differ' because i ignore files added or removed. i don't care about those.
+cat changedFiles | grep 'differ' | cut -d' ' -f2,4 | xargs -L1 diff > diffLines
